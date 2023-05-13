a1b533f29dc511cb2702debd234b27520451dfa1
Aletris Pinkroot <aletris@aletrispinkroot.com>
2023-05-13 17:03:34 +1000

    base

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/index.css';
+
+const IndexPage = () => {
+	return (
+		<div className='splash-container'>
+			<h1 className='splash-text'>Spencer Raymond</h1>
+			<p className='splash-text'>coming soon</p>
+		</div>
+	);
+};
+
+export default IndexPage;
