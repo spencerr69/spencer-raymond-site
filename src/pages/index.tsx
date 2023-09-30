446dd2b87d992dd2d1e7aeb91021f0e1ede3eedc
Aletris Pinkroot <72479926+AletrisPinkroot@users.noreply.github.com>
2023-09-30 19:59:23 +1000

    better looking

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import '../styles/index.css';
 
+const description =
+	'Official Website for Spencer Raymond | Spencer Raymond is an Indie-Pop artist from Melbourne, Australia.';
+
 const IndexPage = () => {
 	return (
-		<div className='splash-container'>
+		<div className='splash-container splash-bg'>
 			<h1 className='splash-text'>Spencer Raymond</h1>
-			<p className='splash-text'>coming soon</p>
+			<p className='splash-text'>2024</p>
 		</div>
 	);
 };
@@ -15,17 +18,26 @@ export default IndexPage;
 export function Head() {
 	return (
 		<>
-			<meta
-				name='description'
-				content='Official Website for Spencer Raymond | Spencer Raymond is a Pop act from Melbourne, Australia.'
-			/>
+			<meta name='description' content={description} />
 			<meta name='twitter:card' content='summary' />
-			<meta name='twitter:site' content='spencerr69' />
+			<meta name='twitter:creator' content='spencerr69' />
 			<meta name='twitter:title' content='Spencer Raymond' />
-			<meta
-				name='twitter:description'
-				content='Official Website for Spencer Raymond | Spencer Raymond is a Pop act from Melbourne, Australia.'></meta>
+			<meta name='twitter:url' content='https://spencerraymon.de/' />
+			<meta name='twitter:description' content={description}></meta>
 			<title>Spencer Raymond</title>
 		</>
 	);
 }
+
+const filter = (
+	<>
+		<svg>
+			<filter id='noiseFilter'>
+				<feTurbulence
+					type='fractalNoise'
+					baseFrequency='2.3'></feTurbulence>
+			</filter>
+			<rect width='100%' height='100%' filter='url(#noiseFilter)'></rect>
+		</svg>
+	</>
+);
