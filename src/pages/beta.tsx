ec9c4a490107c40a290f2683cdd2bc6d7d4d9c9d
Spencer Raymond <spencer@spencerraymon.de>
2025-10-11 17:59:02 +1100

    updates again

---
+++ src/pages/beta.tsx
@@ -0,0 +1,18 @@
+import React, { ReactNode, useEffect, useState } from "react";
+import "../styles/beta.scss";
+
+const BetaPage = () => {
+    return (
+        <>
+            <div className="background">
+                <div className="splash-area">
+                    <h1>stuck in the stream</h1>
+                </div>
+                <div className="divider"></div>
+                <div className="discog"></div>
+            </div>
+        </>
+    );
+};
+
+export default BetaPage;
